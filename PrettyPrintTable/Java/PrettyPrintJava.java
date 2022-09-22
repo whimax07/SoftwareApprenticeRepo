fa0ffdbce2ba79ddd5b9bfa79b7c79d0debf9695
whimax07 <59751270+whimax07@users.noreply.github.com>
2022-09-22 22:58:17 +0100

    Added an inner class and had some fun with streams.

--- PrettyPrintTable/Java/PrettyPrintJava.java
+++ PrettyPrintTable/Java/PrettyPrintJava.java
@@ -1,9 +1,21 @@
+import java.util.ArrayList;
 
 class PrettyPrintJava {
 
+    public static void printTable(String[] headers, ArrayList<Object[]> data) {
+
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        String[] headers = new String[]{"Name", "Language", "OS"};
+
+        ArrayList<Object[]> data = new ArrayList<>();
+        data.add(new Object[]{"a"});
+        data.add(new Object[]{"b", "c"});
+
+        printTable(new String[]{}, data);
+
+
     }
 
 }
---
+++ PrettyPrintTable/Java/Table.java
@@ -0,0 +1,126 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.stream.IntStream;
+
+public class Table {
+
+    private final ArrayList<String> headers;
+
+    private final int numHeaders;
+
+    private ArrayList<ArrayList<String>> data;
+
+
+
+    public Table(ArrayList<String> headers) {
+        this.headers = headers;
+        numHeaders = headers.size();
+    }
+
+
+
+    /**
+     * This is the no expect version of the add a row to the table function.
+     * @return false is the row has a different number of entries compared to headers.
+     */
+    public boolean addRow(ArrayList<String> row) {
+        if (row.size() == numHeaders) {
+            data.add(row);
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * This is the no expect version of the add some number of rows to the table function.
+     * @return false if any of the rows has a different number of entries compared to headers.
+     */
+    public boolean addRows(ArrayList<ArrayList<String>> rows) {
+        if (rows.size() == 0) {
+            return true;
+        }
+
+        for (var row : rows) {
+            if (row.size() != numHeaders) {
+                return false;
+            }
+        }
+
+        data.addAll(rows);
+        return true;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return new StringifyTable().builder.toString();
+    }
+
+
+    private class StringifyTable {
+
+        public StringBuilder builder = new StringBuilder();
+
+        private ArrayList<Integer> headerWidths = new ArrayList<>();
+
+
+
+        public StringifyTable() {
+            findColumnWidths();
+
+            addLine();
+            addHeaders();
+            addLine();
+            addData();
+            addLine();
+        }
+
+
+
+        private void findColumnWidths() {
+            for (int i = 0; i < numHeaders; i++) {
+                // NOTE(Max): There are a couple of different ways to write the find bit that finds the width of a
+                // column, I've done a couple of them. The 3rd one is more of an example of how streams work.
+
+                // 1) The normal way.
+                int width_a = headers.get(i).length();
+                for (var row : data) {
+                    width_a = Math.max(width_a, row.get(i).length());
+                }
+                headerWidths.set(i, width_a);
+
+
+                final int finalI = i;
+
+                // 2) This one I quite like.
+                var width_b = data.stream().mapToInt(row -> row.get(finalI).length()).max().orElse(0);
+                width_b = Math.max(width_b, headers.get(i).length());
+                headerWidths.set(i, width_b);
+
+
+                // 3) This works and is cool, but I can't recommend it.
+                var width_c = data.stream().max(
+                        Comparator.comparingInt(row -> row.get(finalI).length())
+                ).orElse(headers).get(i).length();
+                width_c = Math.max(width_c, headers.get(i).length());
+                headerWidths.set(i, width_c);
+            }
+        }
+
+        private void addLine() {
+
+        }
+
+        private void addHeaders() {
+
+        }
+
+        private void addData() {
+
+        }
+
+    }
+
+}

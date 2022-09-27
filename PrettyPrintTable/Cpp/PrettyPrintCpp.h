3f2b847d0a613c6270d8a26db1ffe2afe995b924
max <m.d.whitehouse@outlook.com>
2022-09-27 13:43:10 +0100

    Did some more work on the cpp version.

--- PrettyPrintTable/Cpp/PrettyPrintCpp.h
+++ PrettyPrintTable/Cpp/PrettyPrintCpp.h
@@ -10,8 +10,15 @@
 #include <array>
 #include <vector>
 #include <algorithm>
+#include <cassert>
+#include <iostream>
 
 namespace pretty {
+
+using Row = std::vector<std::string>;
+using Data = std::vector<Row>;
+using SizeType = unsigned long;
+
 
 // This template gives you compile time guaranties that each row of data has the
 // correct number of entries and that you have provided headers for each column.
@@ -24,15 +31,96 @@ void printTableCompileTime(
 
 }
 
-// You can write this basically the same as the Cc version I am going to use
-// ranges because they are recommended.
-void printTableRuntime(
-        const size_t number_headers,
-        std::vector<std::string> headers,
-        std::vector<std::vector<std::string>> data
+
+std::vector<SizeType> calcWidths(
+        const std::vector<std::string>& headers,
+        const std::vector<std::vector<std::string>>& data
 ) {
-    if (headers.size() != number_headers || )
+    std::vector<SizeType> widths = {};
+
+
+    {
+        std::ranges::for_each(
+                headers,
+                [&widths](auto str) { widths.push_back(str.size()); }
+        );
+
+
+        auto max = [](auto a, auto b) { return std::max(a, b); };
+        auto strSize = [](const auto &str) { return str.size(); };
+        auto checkRow = [&](const auto &row) {
+            std::ranges::transform(
+                    // Inputs.
+                    row, widths,
+                    // Output.
+                    widths.begin(),
+                    // Operation to make the result.
+                    max,
+                    // Project the inputs.
+                    strSize, {}
+            );
+        };
+        std::ranges::for_each(data, checkRow);
+    }
+
+
+    {
+        for (const auto& header : headers) {
+            widths.push_back(headers.size());
+        }
+
+        for (const auto& row : data) {
+            auto strI = row.begin();
+            auto widthI = widths.begin();
+            for ( ; widthI < widths.end(); ++ strI, ++ widthI) {
+                if (*widthI < strI->size()) {
+                    *widthI = strI->size();
+                }
+            }
+        }
+    }
+
+    return widths;
 }
+
+
+void drawRow(const std::vector<SizeType>& widths, const Row& row) {
+    std::cout << typeid(widths).name();
+}
+
+
+void drawData(const std::vector<SizeType>& widths, const Data& data) {
+    std::for_each(
+            data.begin(),
+            data.end(),
+            [widths](const auto& row) { drawRow(widths, row); }
+    );
+}
+
+
+void printTable(const Row& headers, const Data& data) {
+    auto num_headers = headers.size();
+
+    // All rows must have the same number of items.
+    assert(std::ranges::all_of(
+            data,
+            [num_headers](const auto& row) { return row.size() == num_headers; }
+    ));
+
+    std::vector<SizeType> widths = calcWidths(headers, data);
+//    drawBar(widths);
+    drawRow(widths, headers);
+//    drawBar(widths);
+    drawData(widths, data);
+//    drawBar(widths);
+}
+
+
+
+
+
+
+
 
 }
 
